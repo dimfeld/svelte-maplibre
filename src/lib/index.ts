01a9b3b971aa8109fe743f3aee4572e007214631
Daniel Imfeld <daniel@imfeld.dev>
2023-02-20 10:39:23 -1000

    add exports

---
+++ src/lib/index.ts
@@ -0,0 +1,9 @@
+export { default as Map } from './Map.svelte';
+export { default as Marker } from './Marker.svelte';
+export {
+  getId,
+  mapContext,
+  updatedSourceContext,
+  updatedLayerContext,
+  updatedMarkerContext,
+} from './context.js';
