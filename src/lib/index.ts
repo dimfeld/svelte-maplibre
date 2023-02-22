35627a5f9c77ff6e03cb4f7a60f1355c037cc77a
Daniel Imfeld <daniel@imfeld.dev>
2023-02-22 13:47:49 -1000

    export all components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,16 @@
+export { default as CircleLayer } from './CircleLayer.svelte';
+export { default as Control } from './Control.svelte';
+export { default as ControlButton } from './ControlButton.svelte';
+export { default as ControlGroup } from './ControlGroup.svelte';
+export { default as FillExtrusionLayer } from './FillExtrusionLayer.svelte';
+export { default as FillLayer } from './FillLayer.svelte';
+export { default as GeoJSON } from './GeoJSON.svelte';
+export { default as HeatmapLayer } from './HeatmapLayer.svelte';
+export { default as Layer } from './Layer.svelte';
+export { default as LineLayer } from './LineLayer.svelte';
 export { default as Map } from './Map.svelte';
 export { default as Marker } from './Marker.svelte';
+export { default as SymbolLayer } from './SymbolLayer.svelte';
 export {
   getId,
   mapContext,
@@ -7,3 +18,6 @@ export {
   updatedLayerContext,
   updatedMarkerContext,
 } from './context.js';
+export * from './expressions.js';
+export * from './filters.js';
+export * from './types.js';
