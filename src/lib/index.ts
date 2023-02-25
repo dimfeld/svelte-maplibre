ff86c2d7da12b1fc43293a6c5e640c9bfac71e7a
Daniel Imfeld <daniel@imfeld.dev>
2023-02-24 14:28:37 -1000

    v0.1.3

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -13,6 +13,7 @@ export { default as Layer } from './Layer.svelte';
 export { default as LineLayer } from './LineLayer.svelte';
 export { default as Map } from './Map.svelte';
 export { default as Marker } from './Marker.svelte';
+export { default as MarkerLayer } from './MarkerLayer.svelte';
 export { default as NavigationControl } from './NavigationControl.svelte';
 export { default as Popup } from './Popup.svelte';
 export { default as ScaleControl } from './ScaleControl.svelte';
