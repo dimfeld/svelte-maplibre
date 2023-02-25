49a3a4dd011f37e7b88037c6105f7283d467a00b
Daniel Imfeld <daniel@imfeld.dev>
2023-02-24 17:45:53 -1000

    update with real numbers

--- src/routes/examples/geojson_extrusion/+page.svelte
+++ src/routes/examples/geojson_extrusion/+page.svelte
@@ -7,20 +7,11 @@
   import cbsa from '$site/cbsa.json';
   import FillExtrusionLayer from '$lib/FillExtrusionLayer.svelte';
   import Popup from '$lib/Popup.svelte';
-
-  function generateData() {
-    for (let feature of cbsa.features) {
-      // Placeholder until I get real data in here.
-      feature.properties['population'] = Math.ceil(Math.random() * 1000);
-    }
-
-    return cbsa;
-  }
 </script>
 
 <p>
-  Population for US Metropolitan Areas. Population values are currently random, pending import of
-  real data.
+  Map of US Metropolitan Areas. Height indicates total population and color indicates population
+  density.
 </p>
 <Map
   style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
@@ -30,28 +21,29 @@
   center={[-98.137, 40.137]}
   zoom={4}
 >
-  <GeoJSON id="cbsa" data={generateData()} promoteId="CBSAFP">
+  <GeoJSON id="cbsa" data={cbsa} promoteId="CBSAFP">
     <FillExtrusionLayer
       paint={{
         'fill-extrusion-base': 0,
         'fill-extrusion-color': [
           'interpolate',
           ['linear'],
-          ['get', 'population'],
+          // Population density
+          ['/', ['get', 'POPESTIMATE2020'], ['/', ['get', 'ALAND'], 1000000]],
           0,
-          '#aaa',
-          1000,
           '#0a0',
+          200,
+          '#a00',
         ],
         'fill-extrusion-opacity': 0.6,
-        'fill-extrusion-height': ['*', ['get', 'population'], 100],
+        'fill-extrusion-height': ['/', ['get', 'POPESTIMATE2020'], 20],
       }}
       beforeLayerType="symbol"
     >
       <Popup openOn="hover" let:features>
         <div class="flex flex-col gap-2">
           <div class="text-lg font-bold">{features[0].properties.NAME}</div>
-          <p>Population: {features[0].properties.population}</p>
+          <p>Population: {features[0].properties.POPESTIMATE2020}</p>
         </div>
       </Popup>
     </FillExtrusionLayer>
