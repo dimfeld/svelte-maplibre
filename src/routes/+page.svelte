16a0c4944fe770fe8f9be45be164a0ab37a9ff7b
Daniel Imfeld <daniel@imfeld.dev>
2023-02-24 14:26:48 -1000

    add to front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,14 @@
   <h1>Examples</h1>
 
   <ul>
-    <li><a href="/examples/basic">Basic</a></li>
+    <li><a href="/examples/basic">Plain Map</a></li>
     <li><a href="/examples/custom_marker">Custom Marker</a></li>
     <li><a href="/examples/geojson_polygon">GeoJSON Filled Polygon</a></li>
     <li><a href="/examples/geojson_line_layer">Styled Line</a></li>
     <li><a href="/examples/heatmap">Heatmap</a></li>
     <li><a href="/examples/controls">Controls</a></li>
     <li><a href="/examples/clusters">Clusters and Popups</a></li>
+    <li><a href="/examples/custom_marker_clusters">Custom Markers and Clusters</a></li>
     <li><a href="/examples/image_symbols">Images in a Symbol Layer</a></li>
     <li><a href="/examples/marker_layer">Layer Consisting of Markers</a></li>
     <li><a href="/examples/3d_buildings">3D Buildings</a></li>
