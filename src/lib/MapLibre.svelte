806cb09127e823e22f77175d19927ce178bf9cd2
Daniel Imfeld <daniel@imfeld.dev>
2023-09-04 09:30:09 -1000

    better comment on diff caveats

--- src/lib/MapLibre.svelte
+++ src/lib/MapLibre.svelte
@@ -16,8 +16,8 @@
   /** The style to use for the map. */
   export let style: string | maplibregl.StyleSpecification;
   /** Tell MapLibre to update the map in place when changing the style, diffing the old style against the new one to
-   * make minimal changes. In some cases, such as when just switching between two different styles, MapLibre may not
-   * process an update as expected if this is enabled, due to https://github.com/maplibre/maplibre-gl-js/issues/2651. */
+   * make minimal changes. If you enable this, be aware of https://github.com/maplibre/maplibre-gl-js/issues/2651,
+   * which may prevent some style changes from becoming visible when diffing is enabled. */
   export let diffStyleUpdates = false;
   export let center: LngLatLike = [0, 0];
   export let zoom = 1;
