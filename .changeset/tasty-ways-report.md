721b0a0929a23a5308e4bd2e91365500ab2b0fa4
Stuart Lynn <stuart.lynn@gmail.com>
2023-09-25 02:39:29 +0100

    Adds a VectorTileSource to enable non base map MVTs and PMTile Sources (#59)
    * Adds a VectorTileSource to enable non base map MVTs and PMTile Sources
    
    * Removing stray comment
    
    * Removing stray dep
    
    * Fix for import bug
    
    * add changeset
    
    ---------
    
    Co-authored-by: Daniel Imfeld <daniel@imfeld.dev>

---
+++ .changeset/tasty-ways-report.md
@@ -0,0 +1,5 @@
+---
+'svelte-maplibre': patch
+---
+
+Add support for vector tile sources
---
+++ src/lib/VectorTileSource.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { onDestroy, tick } from 'svelte';
+  import { getId, updatedSourceContext } from './';
+  import * as pmtiles from 'pmtiles';
+  import maplibregl from 'maplibre-gl';
+  import flush from 'just-flush';
+
+  export let id: string = getId('vector');
+  export let url: string | null = null;
+  export let tiles: Array<string> | null = null;
+
+  if (url && url.includes('pmtiles://')) {
+    if (!maplibregl.config.REGISTERED_PROTOCOLS.hasOwnProperty('pmtiles')) {
+      let protocol = new pmtiles.Protocol();
+      maplibregl.addProtocol('pmtiles', protocol.tile);
+    }
+  }
+
+  const { map, self: source } = updatedSourceContext();
+
+  $: if ($map && $source !== id) {
+    if ($source) {
+      $map.removeSource($source);
+    }
+
+    $source = id;
+    $map.addSource(
+      $source,
+      flush({
+        type: 'vector',
+        url,
+        tiles,
+      })
+    );
+  }
+
+  onDestroy(() => {
+    if ($source) {
+      let sourceName = $source;
+      $source = null;
+      tick().then(() => {
+        // Check if the map is still loaded. If the entire map was being torn down
+        // then we don't want to call any other functions on it.
+        if ($map?.loaded()) {
+          $map?.removeSource(sourceName);
+        }
+      });
+    }
+  });
+</script>
+
+{#if $source}
+  {#key $source}
+    <slot />
+  {/key}
+{/if}
