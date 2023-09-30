7683fe430e7322ede56bc012d68da277ed402cd3
Stuart Lynn <stuart.lynn@gmail.com>
2023-09-30 17:08:07 +0100

    Removing console.log

--- src/lib/JoinedData.svelte
+++ src/lib/JoinedData.svelte
@@ -6,7 +6,6 @@
 
 	const { map, source } = updatedLayerContext();
 	$: if (data && $map && $source) {
-		console.log('Assigning data ', data, $map, $source);
 		for (const row of data) {
 			const id = row[idCol];
 			$map.setFeatureState({ id, source: $source, sourceLayer }, row);
