13012d3ca0597bd58cc91f3c2623a96a37c30fdb
Lars Maxfield <maxfield.lars@gmail.com>
2024-05-13 12:13:25 +0200

    Add to Examples a 3D Terrain page which closely mirrors the MapLibre demo of 3D Terrain; add modifiable elements to that page for hillshade and terrain settings

---
+++ src/routes/examples/3d_terrain/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+  import MapLibre from '$lib/MapLibre.svelte';
+  import NavigationControl from '$lib/NavigationControl.svelte';
+  import RasterTileSource from '$lib/RasterTileSource.svelte';
+  import RasterDEMTileSource from '$lib/RasterDEMTileSource.svelte';
+  import RasterLayer from '$lib/RasterLayer.svelte';
+  import HillshadeLayer from '$lib/HillshadeLayer.svelte';
+  import Terrain from '$lib/Terrain.svelte';
+  import TerrainControl from '$lib/TerrainControl.svelte';
+  import CodeSample from '$site/CodeSample.svelte';
+  import code from './+page.svelte?raw';
+  import type { PageData } from './$types';
+  import type { PropertyValueSpecification } from 'maplibre-gl';
+
+  export let data: PageData;
+
+  let terrainExaggeration: number = 1.0;
+  let hillshadeExaggeration: number = 0.5;
+  let illuminationAnchor: PropertyValueSpecification<"map" | "viewport"> = "map";
+  
+</script>
+
+<p>
+  This map shows how to use 3D terrain with hillshading on the map. Data from <a
+    href="https://github.com/maplibre/demotiles"
+    target="_blank">MapLibre Demo Tiles</a
+  >. Tutorial based on <a
+    href="https://maplibre.org/maplibre-gl-js/docs/examples/3d-terrain/"
+    target="_blank">MapLibre GL JS 3D Terrain</a
+  >. 
+</p>
+
+<fieldset class="flex gap-x-4">
+  <legend>Hillshade illumination anchor</legend>
+  <label><input type="radio" bind:group={illuminationAnchor} value="map" /> Map</label>
+  <label><input type="radio" bind:group={illuminationAnchor} value="viewport" /> Viewport (default)</label>
+</fieldset>
+
+<fieldset class="flex gap-x-4">
+  <legend>Exaggeration</legend>
+  <label>
+    Hillshade: {hillshadeExaggeration.toFixed(2)}
+    <input type="range" min="0.0" max="1.0" step="0.01" bind:value={hillshadeExaggeration} id="hillshade-exaggeration">
+  </label>
+  <label>
+    Terrain: {terrainExaggeration.toFixed(1)}
+    <input type="range" min="0.0" max="5.0" step="0.1" bind:value={terrainExaggeration} id="terrain-exaggeration">
+  </label>
+</fieldset>
+
+<MapLibre
+  style={{
+    version: 8,
+    center: [11.39085, 47.30000],
+    zoom: 12,
+    pitch: 52,
+    sources: {},
+    layers: [],
+  }}
+  class="relative aspect-[9/16] max-h-[70vh] w-full sm:aspect-video sm:max-h-full"
+  diffStyleUpdates={true}
+>
+  <NavigationControl visualizePitch={true} />
+  <RasterTileSource tiles={['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png']} tileSize={256}>
+    <RasterLayer paint={{}} />
+  </RasterTileSource>
+  <!-- Use a different source for terrain and hillshade layers, to improve render quality -->
+  <RasterDEMTileSource 
+    tiles={['https://demotiles.maplibre.org/terrain-tiles/{z}/{x}/{y}.png']}
+    tileSize={256}
+    id='terrainSource'
+  />
+  <RasterDEMTileSource
+    tiles={['https://demotiles.maplibre.org/terrain-tiles/{z}/{x}/{y}.png']}
+    tileSize={256}
+    id='hillshadeSource'
+  >
+    <HillshadeLayer 
+      id={'hills'} 
+      layout={{ visibility: 'visible' }} 
+      paint={{
+        'hillshade-exaggeration': hillshadeExaggeration,
+        'hillshade-illumination-anchor': illuminationAnchor,
+        'hillshade-shadow-color': '#473B24',
+        }}
+    />
+  </RasterDEMTileSource>
+  <Terrain source={'terrainSource'} exaggeration={terrainExaggeration} />
+  <TerrainControl source={'terrainSource'} exaggeration={terrainExaggeration} />
+</MapLibre>
+
+<CodeSample {code} startBoundary="<MapLibre"/>
