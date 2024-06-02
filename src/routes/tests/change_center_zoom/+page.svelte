74d5c91aff3ad1f62937fd11e3623f8ca1e40ed9
Daniel Imfeld <daniel@imfeld.dev>
2024-06-02 13:16:47 -1000

    add more changes to change_center_zoom test

--- src/routes/tests/change_center_zoom/+page.svelte
+++ src/routes/tests/change_center_zoom/+page.svelte
@@ -1,45 +1,88 @@
 <script lang="ts">
-  import type { LngLatLike } from 'maplibre-gl';
+  import type { LngLatLike, MapMouseEvent } from 'maplibre-gl';
   import MapLibre from '$lib/MapLibre.svelte';
   import CodeSample from '$site/CodeSample.svelte';
   import code from './+page.svelte?raw';
 
-  let center: LngLatLike = [0, 0];
-  let zoom = 2;
+  let coords = [
+    {
+      center: [116, -34],
+      zoom: 8,
+      pitch: 30,
+      bearing: 30,
+    },
+    {
+      center: [-107, 40],
+      zoom: 3,
+      pitch: 0,
+      bearing: 210,
+    },
+  ] as { center: [number, number]; zoom: number; pitch: number; bearing: number }[];
 
-  let newZoom = zoom;
-  let newCenter = center;
+  let center: LngLatLike = coords[0].center;
+  let zoom = coords[0].zoom;
+  let bearing = coords[0].bearing;
+  let pitch = coords[0].pitch;
 
-  function apply() {
+  let currentIndex = 0;
+  function toggle() {
+    currentIndex = (currentIndex + 1) % coords.length;
+    const {
+      center: newCenter,
+      zoom: newZoom,
+      bearing: newBearing,
+      pitch: newPitch,
+    } = coords[currentIndex];
     center = newCenter;
     zoom = newZoom;
+    bearing = newBearing;
+    pitch = newPitch;
   }
+
+  function quickToggle() {
+    toggle();
+    setTimeout(() => toggle(), 200);
+  }
+
+  let currentCoords = center;
+  let currentZoom = zoom;
+  let currentBearing = bearing;
+  let currentPitch = pitch;
+
+  function handleMoveEnd(ev: CustomEvent<MapMouseEvent>) {
+    const map = ev.detail.target;
+    let center = map.getCenter();
+    currentCoords = [center.lng, center.lat];
+    currentZoom = map.getZoom();
+    currentBearing = map.getBearing();
+    currentPitch = map.getPitch();
+  }
+
+  // when working correctly, the Toggle button should successfully change all the values, and Quick Toggle should end up
+  // back at the current coordinates.
 </script>
 
 <div class="flex gap-4">
-  <label>
-    Longitude
-    <input type="number" class="input" bind:value={newCenter[0]} />
-  </label>
-  <label>
-    Latitude
-    <input type="number" class="input" bind:value={newCenter[1]} />
-  </label>
-  <label>
-    Zoom
-    <input type="number" class="input" bind:value={newZoom} />
-  </label>
-</div>
+  <div class="flex gap-4">
+    <button class="btn variant-filled mb-4" type="button" on:click={toggle}> Toggle </button>
 
-<button class="btn variant-filled mb-4 mt-2" type="button" on:click={apply}
-  >Apply new Coordinates</button
->
+    <button class="btn variant-filled mb-4" type="button" on:click={quickToggle}>
+      Toggle and Back
+    </button>
+  </div>
+  <div>
+    {currentCoords[0].toFixed(3)}, {currentCoords[1].toFixed(3)} @ {currentZoom}, {currentBearing}/{currentPitch}
+  </div>
+</div>
 
 <MapLibre
   style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
   class="relative aspect-[9/16] max-h-[70vh] w-full sm:aspect-video sm:max-h-full"
-  bind:center
-  bind:zoom
+  on:moveend={handleMoveEnd}
+  {bearing}
+  {pitch}
+  {center}
+  {zoom}
   standardControls
 ></MapLibre>
 
