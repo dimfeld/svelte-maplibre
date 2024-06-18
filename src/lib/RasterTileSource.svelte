14991b92509016cdc0b2fc22b8756e1cb80fa611
ADD-William-WaltersDavis <william.waltersdavis@dft.gov.uk>
2024-06-18 17:54:06 +0100

    Add pmtiles support for raster url

--- src/lib/RasterTileSource.svelte
+++ src/lib/RasterTileSource.svelte
@@ -4,17 +4,26 @@
   import { addSource, removeSource } from './source.js';
   import type { Scheme } from './types.js';
   import flush from 'just-flush';
-  import type { RasterTileSource } from 'maplibre-gl';
+  import * as pmtiles from 'pmtiles';
+  import maplibregl, { type RasterTileSource } from 'maplibre-gl';
 
   export let id: string = getId('raster-source');
-  export let tiles: string[];
+  export let tiles: string[] | null = null;
   export let tileSize: number | undefined = undefined;
+  export let url: string;
   export let bounds: Array<number> | null = null;
   export let scheme: Scheme | null = null;
   export let attribution: string | null = null;
   export let minzoom: number | null = null;
   export let maxzoom: number | null = null;
   export let volatile: boolean | null = null;
+
+  if (url && url.includes('pmtiles://')) {
+    if (!maplibregl.config.REGISTERED_PROTOCOLS.hasOwnProperty('pmtiles')) {
+      let protocol = new pmtiles.Protocol();
+      maplibregl.addProtocol('pmtiles', protocol.tile);
+    }
+  }
 
   const { map, self: source } = updatedSourceContext();
   let sourceObj: RasterTileSource | undefined;
@@ -29,6 +38,7 @@
         type: 'raster',
         tiles,
         tileSize,
+        url,
         bounds,
         scheme,
         attribution,
@@ -50,12 +60,14 @@
     sourceObj;
   }
 
-  // Don't set tiles again after we've just created it.
+  // Don't set tiles/url again after we've just created it.
   $: if (sourceObj) {
     if (first) {
       first = false;
-    } else {
+    } else if (tiles) {
       sourceObj.setTiles(tiles);
+    } else {
+      sourceObj.setUrl(url);
     }
   }
 
